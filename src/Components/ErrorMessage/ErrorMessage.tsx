46a0d028a11b10e4e7abbbc82b993fa9e310cf87
George Mikheev <GeorgeMikheev56@yandex.ru>
2025-02-08 01:44:35 +0500

    add: ErrorMessage

---
+++ src/Components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+const ErrorMessage = () => (
+	<div className="error">
+		<p className="error__text">
+			Похоже при загрузке данных возникла ошибка. Попробуйте обновить страницу.
+		</p>
+	</div>
+);
+
+export default ErrorMessage;
