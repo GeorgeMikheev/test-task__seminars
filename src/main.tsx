68e736a0c5cc81fb891374bff73db6df3ee2b4a3
George Mikheev <GeorgeMikheev56@yandex.ru>
2025-02-08 01:46:27 +0500

    add: Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {APIProvider} from './context/APIContext'
 import App from './Components/App/App'
 import './index.sass'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <APIProvider>
+      <App />
+    </APIProvider>
   </StrictMode>,
 )
