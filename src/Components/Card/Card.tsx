215bf6a058a71e3bba3f79a82bbe9cfc561ed253
George Mikheev <GeorgeMikheev56@yandex.ru>
2025-02-08 23:29:45 +0500

    fix: change styles for cards

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -17,11 +17,11 @@ const Card = ({ data, props }: { data: TSeminar; props: Iprops }) => {
 			<img className="card__image" src={data.photo} alt={data.title} />
 			<p className="card__description">{data.description}</p>
 			<div className="card__date-time">
-				<time className="card__date" dateTime={data.time}>
-					{data.time}
-				</time>
 				<time className="card__date" dateTime={reverseDate(data.date)}>
 					{reverseDate(data.date)}
+				</time>
+				<time className="card__date" dateTime={data.time}>
+					{data.time}
 				</time>
 			</div>
 			<div className="card__buttons">
