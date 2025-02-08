5d970e3de4ea156bff0e1f14dbf5ec07c5089137
George Mikheev <GeorgeMikheev56@yandex.ru>
2025-02-08 23:34:09 +0500

    add: error page

--- src/Components/ErrorMessage/ErrorMessage.tsx
+++ src/Components/ErrorMessage/ErrorMessage.tsx
@@ -1,5 +1,8 @@
+import "./ErrorMessage.sass";
+
 const ErrorMessage = () => (
 	<div className="error">
+		<h2 className="error__title">Error</h2>
 		<p className="error__text">
 			Похоже при загрузке данных возникла ошибка. Попробуйте обновить страницу.
 		</p>
