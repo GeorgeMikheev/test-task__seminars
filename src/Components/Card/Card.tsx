c5bfc75939d9f28d05e8128271d9a582b1725cc3
George Mikheev <GeorgeMikheev56@yandex.ru>
2025-02-08 23:39:03 +0500

    fix: change layout cards

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -14,7 +14,9 @@ const Card = ({ data, props }: { data: TSeminar; props: Iprops }) => {
 	return (
 		<div className="card">
 			<h3 className="card__title">{data.title}</h3>
-			<img className="card__image" src={data.photo} alt={data.title} />
+			<div className="card__image-block">
+				<img className="card__image" src={data.photo} alt={data.title} />
+			</div>
 			<p className="card__description">{data.description}</p>
 			<div className="card__date-time">
 				<time className="card__date" dateTime={reverseDate(data.date)}>
